80880ee41210ace0d5f293a202b2afeb33760065
Josh Bode <joshbode@gmail.com>
2014-05-09 00:48:55 +1000

    Data Type Conversions
    Data type conversions for specialised numeric (integer and boolean) and
    non-character data types such as dates.
    
    Uses Java SQL data type codes to map data rather than hard-coded flags.

--- R/class.R
+++ R/class.R
@@ -77,7 +77,7 @@ setMethod("dbDisconnect", "JDBCConnection", def=function(conn, ...)
 
 setMethod("dbSendQuery", signature(conn="JDBCConnection", statement="character"),  def=function(conn, statement, ..., list=NULL) {
   statement <- as.character(statement)[1L]
-  ## if the statement starts with {call or {?= call then we use CallableStatement 
+  ## if the statement starts with {call or {?= call then we use CallableStatement
   if (isTRUE(as.logical(grepl("^\\{(call|\\?= *call)", statement)))) {
     s <- .jcall(conn@jc, "Ljava/sql/CallableStatement;", "prepareCall", statement, check=FALSE)
     .verify.JDBC.result(s, "Unable to execute JDBC callable statement ",statement)
@@ -97,7 +97,7 @@ setMethod("dbSendQuery", signature(conn="JDBCConnection", statement="character")
     .verify.JDBC.result(s, "Unable to create simple JDBC statement ",statement)
     r <- .jcall(s, "Ljava/sql/ResultSet;", "executeQuery", as.character(statement)[1], check=FALSE)
     .verify.JDBC.result(r, "Unable to retrieve JDBC result set for ",statement)
-  } 
+  }
   md <- .jcall(r, "Ljava/sql/ResultSetMetaData;", "getMetaData", check=FALSE)
   .verify.JDBC.result(md, "Unable to retrieve JDBC result set meta data for ",statement, " in dbSendQuery")
   new("JDBCResult", jr=r, md=md, stat=s, pull=.jnull())
@@ -107,7 +107,7 @@ if (is.null(getGeneric("dbSendUpdate"))) setGeneric("dbSendUpdate", function(con
 
 setMethod("dbSendUpdate",  signature(conn="JDBCConnection", statement="character"),  def=function(conn, statement, ..., list=NULL, max.batch=10000L) {
   statement <- as.character(statement)[1L]
-  ## if the statement starts with {call or {?= call then we use CallableStatement 
+  ## if the statement starts with {call or {?= call then we use CallableStatement
   if (isTRUE(as.logical(grepl("^\\{(call|\\?= *call)", statement)))) {
     s <- .jcall(conn@jc, "Ljava/sql/CallableStatement;", "prepareCall", statement, check=FALSE)
     .verify.JDBC.result(s, "Unable to execute JDBC callable statement ",statement)
@@ -309,10 +309,12 @@ setMethod("fetch", signature(res="JDBCResult", n="numeric"), def=function(res, n
   if (length(block) != 1L) stop("invalid block size")
   if (cols < 1L) return(NULL)
   l <- list()
+  col_types <- list()
   cts <- rep(0L, cols)
   for (i in 1:cols) {
-    ct <- .jcall(res@md, "I", "getColumnType", i)
-    if (ct == -5 | ct ==-6 | (ct >= 2 & ct <= 8)) {
+    ct <- .get.type(.jcall(res@md, "I", "getColumnType", i))
+    col_types[[i]] <- ct
+    if (ct %in% c('integer', 'double', 'logical')) {
       l[[i]] <- numeric()
       cts[i] <- 1L
     } else
@@ -336,6 +338,18 @@ setMethod("fetch", signature(res="JDBCResult", n="numeric"), def=function(res, n
     nrec <- .jcall(rp, "I", "fetch", as.integer(n), block)
     for (i in seq.int(cols)) l[[i]] <- if (cts[i] == 1L) .jcall(rp, "[D", "getDoubles", i) else .jcall(rp, "[Ljava/lang/String;", "getStrings", i)
   }
+  # apply conversions to columns that require it
+  if (RJDBC.options('convert.types')) {
+    conversion_map <- RJDBC.options('conversion.map')
+    for (i in grep('character|numeric', col_types, invert=TRUE)) {
+      conversion <- conversion_map[[col_types[[i]]]]
+      if (is.null(conversion)) {
+        warning(paste0("No conversion specified for '", col_types[[i]], "' data-type."))
+      } else {
+        l[[i]] <- conversion(l[[i]])
+      }
+    }
+  }
   # as.data.frame is expensive - create it on the fly from the list
   attr(l, "row.names") <- c(NA_integer_, length(l[[1]]))
   class(l) <- "data.frame"
@@ -358,11 +372,9 @@ setMethod("dbColumnInfo", "JDBCResult", def = function(res, ...) {
   for (i in 1:cols) {
     l$name[i] <- .jcall(res@md, "S", "getColumnLabel", i)
     l$field.type[i] <- .jcall(res@md, "S", "getColumnTypeName", i)
-    ct <- .jcall(res@md, "I", "getColumnType", i)
-    l$data.type[i] <- if (ct == -5 | ct ==-6 | (ct >= 2 & ct <= 8)) "numeric" else "character"
-    l$field.name[i] <- .jcall(res@md, "S", "getColumnName", i)
+    l$data.type[i] <- .get.type(.jcall(res@md, "I", "getColumnType", i))
   }
-  as.data.frame(l, row.names=1:cols)    
+  as.data.frame(l, row.names=1:cols)
 },
           valueClass = "data.frame")
 
