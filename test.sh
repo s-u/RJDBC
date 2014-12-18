81246ad453dc9b5ee577377611eacfa0993c9342
Simon Urbanek <simon.urbanek@R-project.org>
2014-12-18 17:09:12 -0500

    0.2-5 release

--- test.sh
+++ test.sh
@@ -31,7 +31,7 @@ fetch(r <- dbSendQuery(c, "SELECT count(*) FROM iris"), 1)
 dbGetInfo(r)
 dbHasCompleted(r)
 ## prepared send query
-fetch(dbSendQuery(c, "SELECT Species, count(Species) FROM iris WHERE `Sepal.Width` > ? GROUP BY Species", 3), 1e3)
+fetch(dbSendQuery(c, "SELECT Species, count(Species) FROM iris WHERE `Sepal.Width` > ? GROUP BY Species", 3), 1e3, block=1e3)
 ## simple update
 dbSendUpdate(c, "DROP TABLE IF EXISTS foo")
 dbSendUpdate(c, "CREATE TABLE foo (alpha VARCHAR(32), beta INT)")
