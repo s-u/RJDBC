8810b5efbf7ca2f77b92027dadcee768469590d7
Simon Urbanek <inbox@urbanek.info>
2022-03-24 18:43:15 +1300

    add regression tests to the test suite and instructions for Docker-based test

--- test.sh
+++ test.sh
@@ -4,10 +4,17 @@
 ## you'll need MySQL on the local machine and
 ## adjust the paths/names below as needed
 
-MYSQL=/usr/local/mysql
-DRIVER="$MYSQL/mysql-connector-java.jar"
-DRVCLASS=com.mysql.jdbc.Driver
+DRIVER=mariadb-connector-java.jar
+DRVCLASS=org.mariadb.jdbc.Driver
 
+## NOTE: if needed, you can use Docker for the test as follows:
+## docker run --rm -it -p 3306:3306 -e MARIADB_ROOT_PASSWORD=root mariadb:latest
+
+## We attempt to download the connetor if needed
+if [ ! -e $DRIVER ]; then
+   curl -LO https://dlm.mariadb.com/1965742/Connectors/java/connector-java-2.7.5/mariadb-java-client-2.7.5.jar
+   ln -s mariadb-java-client-2.7.5.jar $DRIVER
+fi
 
 ##---- cut here ----
 
@@ -20,7 +27,22 @@ echo "library(RJDBC)
 ## load driver
 MySQL<-JDBC('$DRVCLASS','$DRIVER',"'"`")
 ## connect
-c<-dbConnect(MySQL, "jdbc:mysql://localhost/test")
+c<-tryCatch(dbConnect(MySQL, "jdbc:mysql://localhost/test", user="test", password="test1234"),
+ ## this is a fall-back for Docker-based test mariadb without any users and testdb
+ error=function(e) {
+    cat("test@localhost failed to connect with: ", as.character(e), "\n")
+    cat("Re-trying with root/root credentials assuming a Docker instance started with \ndocker run --rm -it -p 3306:3306 -e MARIADB_ROOT_PASSWORD=root mariadb:latest\n\n")
+    c0 <- dbConnect(MySQL, "jdbc:mysql://localhost/mysql", user="root", password="root")
+    dbSendUpdate(c0, "CREATE DATABASE IF NOT EXISTS test;")
+    dbSendUpdate(c0, "CREATE USER IF NOT EXISTS test@\"%\" IDENTIFIED BY \"test1234\";")
+    dbSendUpdate(c0, "GRANT ALL ON test.* TO test@\"%\";")
+    dbDisconnect(c0)
+    rm(c0)
+    gc()
+    ## retry after the above
+    dbConnect(MySQL, "jdbc:mysql://localhost/test", user="test", password="test1234")
+ })
+
 ## table operations write/read
 data(iris)
 dbWriteTable(c, "iris", iris, overwrite=TRUE)
@@ -30,9 +52,31 @@ fetch(r <- dbSendQuery(c, "SELECT count(*) FROM iris"), 1)
 ## query info - some people need this
 dbGetInfo(r)
 dbHasCompleted(r)
+## you have to read more than what is tehre
+fetch(r, 1)
+dbGetInfo(r)
+dbHasCompleted(r)
 ## prepared send query
 fetch(r <- dbSendQuery(c, "SELECT Species as Kind, count(Species) as Count FROM iris WHERE `Sepal.Width` > ? GROUP BY Species", 3), 1e3, block=1e3)
 dbColumnInfo(r)
+## append
+i2 <- iris
+i2$Species <- paste0("New.", as.character(i2$Species))
+dbWriteTable(c, "iris", i2, append=TRUE)
+## multi-part fetch
+r <- dbSendQuery(c, "SELECT * FROM iris")
+while (!dbHasCompleted(r)) print(dim(fetch(r, 50)))
+dbClearResult(r)
+i3 <- iris
+i3$Species <- paste0("XXX.", as.character(i3$Species))
+## Regression: named singletons were not inserted
+dbWriteTable(c, "iris", i3[1,], append=TRUE)
+## Regression: merge ... and list=
+dbSendUpdate(c, "INSERT INTO iris VALUES (?, ?, ?, ?, ?)", i3[[1]][2], list=i3[2,-1])
+## same but batch-insert
+dbSendUpdate(c, "INSERT INTO iris VALUES (?, ?, ?, ?, ?)", i3[[1]][3:5], list=i3[3:5,-1])
+dbGetQuery(c, "SELECT Species as Kind, count(Species) as Count FROM iris GROUP BY Species", use.label=TRUE)
+dbGetQuery(c, "SELECT Species as Kind, count(Species) as Count FROM iris GROUP BY Species", use.label=FALSE)
 ## simple update
 dbSendUpdate(c, "DROP TABLE IF EXISTS foo")
 dbSendUpdate(c, "CREATE TABLE foo (alpha VARCHAR(32), beta INT)")
@@ -56,7 +100,12 @@ dbSendUpdate(c, "DROP PROCEDURE bar")
 dbSendUpdate(c, "CREATE PROCEDURE foobar(IN x INT) BEGIN SELECT * FROM foo WHERE beta >= x; END")
 fetch(dbSendQuery(c, "{call foobar(222)}"), -1)
 dbSendUpdate(c, "DROP PROCEDURE foobar")
-
+## remove
+(dbExistsTable(c, "iris"))
+(dbRemoveTable(c, "iris"))
+(dbExistsTable(c, "iris"))
+(dbRemoveTable(c, "iris", silent=TRUE))
+tryCatch(fetch(r <- dbSendQuery(c, "SELECT Species as Kind, count(Species) as Count FROM iris WHERE `Sepal.Width` > ? GROUP BY Species", 3), 1e3, block=1e3), error=function(e) str(e))
 dbDisconnect(c)
 ' | R --vanilla --quiet || echo "*** TEST FAILED ***"
 
